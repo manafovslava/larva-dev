e155d36a4fd77902fc5fe6c120e3634b51036e1d
Viacheslav Manafov <manafovslava@gmail.com>
2023-01-03 13:35:14 +0200

    feat(blackouts.json): added blackouts.json file with array, added fs module to read json file used fs.readFileSync, added more dates to index.js to check program work

--- blackout-time/index.js
+++ blackout-time/index.js
@@ -12,4 +12,10 @@ try {
 printBlackoutTime(new Date('2022-12'));
 printBlackoutTime(new Date('2022-12-02'));
 printBlackoutTime(new Date('2022.12.03'));
-printBlackoutTime(new Date(2022, 11, 4));
+printBlackoutTime(new Date(2022, 12, 4));
+printBlackoutTime(new Date('2022-12-05'));
+printBlackoutTime(new Date('2022-12-06'));
+printBlackoutTime(new Date('2022-12-07'));
+printBlackoutTime(new Date('2022-12-08'));
+printBlackoutTime(new Date('2022-12-09'));
+printBlackoutTime(new Date('2022-12-10'));
--- blackout-time/print-blackout-time.js
+++ blackout-time/print-blackout-time.js
@@ -1,23 +1,32 @@
 import { DateTime } from 'luxon';
+import * as fs from 'fs';
+
+const d = fs.readFileSync('./blackout-time/blackouts.json', 'utf8');
+
+const arry = JSON.parse(d);
+
+const array = arry.arr
+
 
 export const printBlackoutTime = (date) => {
-    if (!(date instanceof Date)) {
-        throw new Error(
-            `Invalid date: ${date}. Unable to print blackout time. Passed 'date' must be an instance of 'Date' class`
-        );
-    }
+   if (!(date instanceof Date)) {
+       throw new Error(
+           `Invalid date: ${date}. Unable to print blackout time. Passed 'date' must be an instance of 'Date' class`
+       );
+   }
 
-    const formatedDate = formatDate(date);
+   const formatedDate = formatDate(date);
 
-    const header = formatedDate === formatDate(new Date())
-        ? `Today: ${formatedDate}, blackouts are:`
-        : `Blackouts on ${formatedDate}:`;
+   const header = formatedDate === formatDate(new Date())
+       ? `Today: ${formatedDate}, blackouts are:`
+       : `Blackouts on ${formatedDate}:`;
 
-    const body = date.getDate() % 2 === 0
-        ? '02:00 - 04:00\n10:00 - 12:00\n18:00 - 20:00'
-        : '06:00 - 08:00\n14:00 - 16:00\n22:00 - 00:00';
+   const body = date.getDate() % array.length === 1
+       ? '00:00 - 02:00\n06:00 - 08:00\n12:00 - 14:00\n18:00 - 20:00'
+       : date.getDate() % array.length === 2 ? '04:00 - 06:00\n10:00 - 12:00\n16:00 - 18:00\n22:00 - 24:00'
+       : '02:00 - 04:00\n08:00 - 10:00\n14:00 - 16:00\n20:00 - 22:00';
 
-    console.log(header + '\n' + body + '\n');
+   console.log(header + '\n' + body + '\n');
 };
 
 const formatDate = (date) => DateTime.fromJSDate(date).toFormat('dd.MM.yyyy');
